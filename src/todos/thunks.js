f4833627eecfe773258f0f38dfab4cc0736e0c05
PupRiku <chris.diorio12@gmail.com>
2021-01-16 16:28:16 -0600

    fix server bug

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -5,14 +5,13 @@ export const loadTodos = () => async (dispatch, getState) => {
         dispatch(loadTodosInProgress());
         const response = await fetch('http://localhost:8080/todos');
         const todos = await response.json();
-
+    
         dispatch(loadTodosSuccess(todos));
     } catch (e) {
         dispatch(loadTodosFailure());
         dispatch(displayAlert(e));
     }
-    
-};
+}
 
 export const displayAlert = text => () => {
     alert(text);
